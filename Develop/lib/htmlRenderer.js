90e555a73ccdcf723e97ae84d7e636789ad69c51
Sean Lester Durham <sean.durham1982@gmail.com>
2023-02-12 20:24:38 -0600

    updated lib folder

--- Develop/lib/htmlRenderer.js
+++ Develop/lib/htmlRenderer.js
@@ -1,3 +1,15 @@
 const path = require('path');
 const fs = require('fs');
 
+const templatesDir = path.resolve(__dirname, "../templates");
+
+const render = employees => {
+    const html = [];
+
+    html.push(employees
+        .filter(employee => employee.getRole() ===))
+
+
+
+
+}
