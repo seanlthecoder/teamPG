8e8a421ec7bf29985434fe7427d4883f2c7a6c83
Sean Lester Durham <sean.durham1982@gmail.com>
2023-02-13 20:38:42 -0600

    updated HTML renderer

--- Develop/lib/htmlRenderer.js
+++ Develop/lib/htmlRenderer.js
@@ -3,13 +3,80 @@ const fs = require('fs');
 
 const templatesDir = path.resolve(__dirname, "../templates");
 
+// the function render takes the employees argument which is an array of employee objects.
+// it uses the empty html array. It will store strings of html code that represent the employees within the array.
 const render = employees => {
     const html = [];
 
+
+
     html.push(employees
-        .filter(employee => employee.getRole() ===))
+        .filter(employee => employee.getRole() === "Engineer")
+        .map(engineer => renderEngineer(engineer))
+      );
+    html.push(employees
+        .filter(employee => employee.getRole() === "Engineer")
+        .map(manager => renderManager(manager))
+
+    );
+    html.push(employees
+        .filter(employee => employee.getRole() === "Intern")
+        .map(intern => renderIntern(intern))
+    );
+    
+    // html variable is an array of strings that represents html elements. 'join' will concatenate the strings into a single string. This string is then passed as an argument to the render function.
+
+    // this code will  render the HTML elements as a single string, which will  display the elements on a webpage or in another context where HTML is supported.
+    return renderHTML_1(html.join(""));
+
+    };
+
+        // the function renderEngineer takes 'engineer' object as an argument. This function takes 'fs' File System, to read the file named "engineer" from the 'templatesDir' directory.  the file is an HTML template to render details about an engineer.
+
+    const renderEngineer = engineer => {
+        let template = fs.readFileSync(path.resolve(templatesDir, "engineer.html"), "utf8");
+        template = replacePlaceholders(template, "role", engineer.getRole());
+        template = replacePlaceholders(template, "name", engineer.getName());
+        template = replacePlaceholders(template, "email", engineer.getEmail());
+        template = replacePlaceholders(template, "id", engineer.getId());
+        template = replacePlaceholders(template, "github", engineer.getGithub());
+
+        return template;
+
+    };
+    
+        const renderIntern = intern => {
+            let template = fs.readFileSync(path.resolve(templatesDir, "intern.html"), "utf8");
+            template = replacePlaceholders(template, "name", intern.getName());
+            template = replacePlaceholders(template, "email", intern.getEmail());
+            template = replacePlaceholders(template, "role", intern.getRole());
+            template = replacePlaceholders(template, "id", intern.getId());
+            template = replacePlaceholders(template, "school", intern.getSchool());
+            return template;
 
 
+        };
+
+        const renderManager = manager => {
+            let template = fs.readFileSync(path.resolve(templatesDir, "manager.html"), "utf8");
+            template = replacePlaceholders(template, "name", manager.getName());
+            template = replacePlaceholders(template, "role", manager.getrole());
+            template = replacePlaceholders(template, "email", manager.getEmail());
+            template = replacePlaceholders(template, "id", manager.getID());
+            template = replacePlaceholders(template, "officeNumber", manager.getOfficeNumber());
+
+            return template;
+        };
+
+        const renderHTML_1 = html => {
+            const template = fs.readFileSync(path.resolve(templatesDir, "HTML_1.html"), "utf8");
+            return replacePlaceholders(template, "team", html);
+        };
+
+        const replacePlaceholders = (template, placeholder, value) => {
+            const pattern = new RegExp("{{ " + placeholder + " }}", "gm");
+            return template.replace(pattern, value);
+        };
 
 
-}
+        module.exports = render;
